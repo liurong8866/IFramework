cecd020caf218f6dcdf0a4ac4790f227b66fc652
liurong8866 <50785330@qq.com>
2021-10-08 01:48:03 +0800

    完成Controller代码生成

--- IFramework.Core/CodeGen/AbstractBind.cs
+++ IFramework.Core/CodeGen/AbstractBind.cs
@@ -1,80 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace IFramework.Core
 {
     public abstract class AbstractBind : MonoBehaviour, IBind
     {
-        [SerializeField] protected BindType bindType = BindType.DefaultElement;
-
-        public string Comment => CustomComment;
-
         public Transform Transform => transform;
 
-        public BindType BindType { get => bindType; set => bindType = value; }
+        public string Comment { get; protected set; }
+
+        public BindType BindType { get; set; } = BindType.DefaultElement;
 
         [HideInInspector] public string CustomComponentName;
 
-        [HideInInspector] public string ComponentGeneratePath;
+        [HideInInspector] public string GeneratePath;
 
-        [HideInInspector] public string CustomComment;
-
-        [HideInInspector] [SerializeField] private string mComponentName;
+        [HideInInspector] [SerializeField] private string componentName;
 
         public virtual string ComponentName {
             get {
-                if (bindType == BindType.DefaultElement) {
-                    if (mComponentName.IsNullOrEmpty()) { mComponentName = GetDefaultComponentName(); }
-                    return mComponentName;
+                if (BindType == BindType.DefaultElement) {
+                    if (componentName.IsNullOrEmpty()) { componentName = GetDefaultComponentName(); }
+                    return componentName;
                 }
                 return CustomComponentName;
             }
-            set => mComponentName = value;
+            set => componentName = value;
         }
 
         private string GetDefaultComponentName()
         {
-            // if (GetComponent<ViewController>()) return GetComponent<ViewController>().GetType().FullName;
-            //
-            // if (GetComponent("SkeletonAnimation")) return "SkeletonAnimation";
-            // if (GetComponent<ScrollRect>()) return "UnityEngine.UI.ScrollRect";
-            // if (GetComponent<InputField>()) return "UnityEngine.UI.InputField";
-            //
-            // // text mesh pro supported
-            // if (GetComponent("TMP.TextMeshProUGUI")) return "TMP.TextMeshProUGUI";
-            // if (GetComponent("TMPro.TextMeshProUGUI")) return "TMPro.TextMeshProUGUI";
-            // if (GetComponent("TMPro.TextMeshPro")) return "TMPro.TextMeshPro";
-            // if (GetComponent("TMPro.TMP_InputField")) return "TMPro.TMP_InputField";
-            //
-            // // ugui bind
-            // if (GetComponent<Dropdown>()) return "UnityEngine.UI.Dropdown";
-            // if (GetComponent<Button>()) return "UnityEngine.UI.Button";
-            // if (GetComponent<Text>()) return "UnityEngine.UI.Text";
-            // if (GetComponent<RawImage>()) return "UnityEngine.UI.RawImage";
-            // if (GetComponent<Toggle>()) return "UnityEngine.UI.Toggle";
-            // if (GetComponent<Slider>()) return "UnityEngine.UI.Slider";
-            // if (GetComponent<Scrollbar>()) return "UnityEngine.UI.Scrollbar";
-            // if (GetComponent<Image>()) return "UnityEngine.UI.Image";
-            // if (GetComponent<ToggleGroup>()) return "UnityEngine.UI.ToggleGroup";
-            //
-            // // other
-            // if (GetComponent<Rigidbody>()) return "Rigidbody";
-            // if (GetComponent<Rigidbody2D>()) return "Rigidbody2D";
-            //
-            // if (GetComponent<BoxCollider2D>()) return "BoxCollider2D";
-            // if (GetComponent<BoxCollider>()) return "BoxCollider";
-            // if (GetComponent<CircleCollider2D>()) return "CircleCollider2D";
-            // if (GetComponent<SphereCollider>()) return "SphereCollider";
-            // if (GetComponent<MeshCollider>()) return "MeshCollider";
-            // if (GetComponent<Collider>()) return "Collider";
-            // if (GetComponent<Collider2D>()) return "Collider2D";
-            //
-            // if (GetComponent<Animator>()) return "Animator";
-            // if (GetComponent<Canvas>()) return "Canvas";
-            // if (GetComponent<Camera>()) return "Camera";
-            // if (GetComponent("Empty4Raycast")) return "QFramework.Empty4Raycast";
-            // if (GetComponent<RectTransform>()) return "RectTransform";
-            // if (GetComponent<MeshRenderer>()) return "MeshRenderer";
-            // if (GetComponent<SpriteRenderer>()) return "SpriteRenderer";
+            if (GetComponent<ViewController>()) return GetComponent<ViewController>().GetType().FullName;
+            
+            if (gameObject.GetComponent<ScrollRect>()) return "UnityEngine.UI.ScrollRect";
+            if (GetComponent<InputField>()) return "UnityEngine.UI.InputField";
+            
+            // ugui bind
+            if (gameObject.GetComponent<Dropdown>()) return "UnityEngine.UI.Dropdown";
+            if (GetComponent<Button>()) return "UnityEngine.UI.Button";
+            if (GetComponent<Text>()) return "UnityEngine.UI.Text";
+            if (GetComponent<RawImage>()) return "UnityEngine.UI.RawImage";
+            if (GetComponent<Toggle>()) return "UnityEngine.UI.Toggle";
+            if (GetComponent<Slider>()) return "UnityEngine.UI.Slider";
+            if (GetComponent<Scrollbar>()) return "UnityEngine.UI.Scrollbar";
+            if (GetComponent<Image>()) return "UnityEngine.UI.Image";
+            if (GetComponent<ToggleGroup>()) return "UnityEngine.UI.ToggleGroup";
+            
+            // other
+            if (GetComponent<Rigidbody>()) return "Rigidbody";
+            if (GetComponent<Rigidbody2D>()) return "Rigidbody2D";
+            
+            if (GetComponent<BoxCollider2D>()) return "BoxCollider2D";
+            if (GetComponent<BoxCollider>()) return "BoxCollider";
+            if (GetComponent<CircleCollider2D>()) return "CircleCollider2D";
+            if (GetComponent<SphereCollider>()) return "SphereCollider";
+            if (GetComponent<MeshCollider>()) return "MeshCollider";
+            if (GetComponent<Collider>()) return "Collider";
+            if (GetComponent<Collider2D>()) return "Collider2D";
+            
+            if (GetComponent<Animator>()) return "Animator";
+            if (GetComponent<Canvas>()) return "Canvas";
+            if (GetComponent<Camera>()) return "Camera";
+            if (GetComponent<RectTransform>()) return "RectTransform";
+            if (GetComponent<MeshRenderer>()) return "MeshRenderer";
+            if (GetComponent<SpriteRenderer>()) return "SpriteRenderer";
+            
+            // text mesh pro supported
+            if (GetComponent("TMP.TextMeshProUGUI")) return "TMP.TextMeshProUGUI";
+            if (GetComponent("TMPro.TextMeshProUGUI")) return "TMPro.TextMeshProUGUI";
+            if (GetComponent("TMPro.TextMeshPro")) return "TMPro.TextMeshPro";
+            if (GetComponent("TMPro.TMP_InputField")) return "TMPro.TMP_InputField";
+            
             return "Transform";
         }
     }
--- IFramework.Unity/Assets/IFramework/Editor/IFramework.Editor/UIKit/GUI/BindInspector.cs
+++ IFramework.Unity/Assets/IFramework/Editor/IFramework.Editor/UIKit/GUI/BindInspector.cs
@@ -1,11 +1,55 @@
+using System;
 using IFramework.Core;
 using UnityEditor;
+using UnityEngine;
 
 namespace IFramework.Editor
 {
-    [CustomEditor(typeof(Bind), true)]
+    [CustomEditor(typeof(AbstractBind), true)]
     public class BindInspector : UnityEditor.Editor
     {
-        
+        private AbstractBind bind => target as AbstractBind;
+
+        private void OnEnable()
+        {
+            bind.ComponentName.IfNullOrEmpty(() => bind.ComponentName = bind.name);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            // // 解决混用Layout重叠问题
+            // EditorGUILayout.GetControlRect();
+            
+            GUILayout.BeginVertical();
+            
+            // 类型
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("类型");
+            bind.BindType = (BindType)EditorGUILayout.EnumPopup(bind.BindType);
+            // bind.BindType = (BindType)EditorGUILayout.EnumFlagsField(bind.BindType);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+
+            // 类名
+            // if (bind.BindType != BindType.DefaultElement) {
+                GUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("类名");
+                bind.ComponentName = EditorGUILayout.TextField(bind.ComponentName);
+                GUILayout.EndHorizontal();
+                GUILayout.Space(5);
+            // }
+            
+            //
+            GUILayout.BeginHorizontal();
+            bind.CustomComment = EditorGUILayout.TextField(bind.CustomComment);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+            //
+            EditorGUILayout.HelpBox("代码生成", MessageType.Info);
+            GUILayout.Space(10);
+            //
+            GUILayout.EndVertical();
+        }
     }
 }
